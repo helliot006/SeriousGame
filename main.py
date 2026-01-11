819766b26bd1fd98b9f5bac7d265e963d5ffce02
helliot006 <elliotlambert006@gmail.com>
2026-01-11 15:12:48 +0100

    First class test (reuf)

--- main.py
+++ main.py
@@ -1,5 +1,61 @@
 import pygame
 
-class main:
+class Main:
     def __init__(self):
         pass 
+
+
+class Perso:
+    def __init__(self, vie, vitesse):
+        self.vie = vie 
+        self.vitesse = vitesse 
+
+
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 1280))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+enemi_pos = pygame.Vector2(screen.get_width() / 3, screen.get_height() / 2)
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("white")
+
+    pygame.draw.circle(screen, "red", player_pos, 40)
+    pygame.draw.circle(screen, "blue", enemi_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_z]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_q]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
+    
+
+
+
+        
